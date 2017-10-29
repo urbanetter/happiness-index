0ac9767c6e65eeae25583ac0e78353e41b59e36a
Urban Etter <uetter@gmail.com>
2017-10-29 15:56:34 +0100

    Renamed index template

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,16 +5,15 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     public function index()
     {
-        return $this->render('default.html.twig');
+        return $this->render('index.html.twig');
     }
 
-    public function happiness(Request $request)
+    public function happiness()
     {
         return new JsonResponse(['result' => true]);
     }
